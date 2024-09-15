84ae2f0e19f09c7b987dfbe156ed3c322a36f3b6
Anna-er <brungilda364@gmail.com>
2024-09-15 17:19:58 +0300

    feat(Stack): Add performance Tests and update readme

--- Stack/README.md
+++ Stack/README.md
@@ -16,4 +16,17 @@
 
 ### Сравнение времени работы Treiber Stack и Elimination Treiber Stack на рандомных операциях на стеке
 
-
+```
+Execution time of Lock-Free Treiber Stack for 1 threads = 44 ms
+Execution time of Lock-Free Treiber Stack for 2 threads = 154 ms
+Execution time of Lock-Free Treiber Stack for 4 threads = 511 ms
+Execution time of Lock-Free Treiber Stack for 8 threads = 1321 ms
+Execution time of Lock-Free Treiber Stack for 12 threads = 2114 ms
+```
+```
+Execution time of Elimination Treiber Stack for 1 threads = 47 ms
+Execution time of Elimination Treiber Stack for 2 threads = 85 ms
+Execution time of Elimination Treiber Stack for 4 threads = 230 ms
+Execution time of Elimination Treiber Stack for 8 threads = 568 ms
+Execution time of Elimination Treiber Stack for 12 threads = 1429 ms
+```
--- Stack/src/test/kotlin/stack/EliminationTreiberStackTest.kt
+++ Stack/src/test/kotlin/stack/EliminationTreiberStackTest.kt
@@ -1,11 +1,14 @@
 package stack
 
 import elimination.EliminationTreiberStack
+import kotlinx.coroutines.*
 import org.jetbrains.kotlinx.lincheck.annotations.Operation
 import org.jetbrains.kotlinx.lincheck.check
 import org.jetbrains.kotlinx.lincheck.LoggingLevel
 import org.jetbrains.kotlinx.lincheck.strategy.stress.StressOptions
 import org.junit.jupiter.api.Test
+import kotlin.random.Random
+import kotlin.system.measureTimeMillis
 
 class EliminationTreiberStackTest {
     private val stack =
@@ -28,4 +31,43 @@ class EliminationTreiberStackTest {
         .actorsPerThread(3)
         .logLevel(LoggingLevel.INFO)
         .check(this::class)
+
+
+    @OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
+    @Test
+    fun performanceTest() {
+        val iterations = 1000000
+        val stack = EliminationTreiberStack<Int>(10)
+        for (threads in mutableListOf(1, 2, 4, 8, 12)) {
+            var executionTime = 0L
+            repeat(10) {
+                val jobs = mutableListOf<Job>()
+                val time = measureTimeMillis {
+                    runBlocking {
+                        repeat(threads) { i ->
+                            jobs.add(launch(newSingleThreadContext(i.toString())) {
+                                repeat(iterations) {
+                                    val j = Random.nextInt(100)
+                                    if (j % 3 == 0) {
+                                        stack.push(Random.nextInt(100))
+                                    }
+                                    if (j % 3 == 1) {
+                                        stack.pop()
+                                    }
+                                    if (j % 3 == 2) {
+                                        stack.top()
+                                    }
+                                }
+                            })
+                        }
+                        jobs.joinAll()
+                    }
+                }
+                executionTime += time
+            }
+            executionTime /= 10
+
+            println("Execution time of Elimination Treiber Stack for $threads threads = $executionTime ms")
+        }
+    }
 }
